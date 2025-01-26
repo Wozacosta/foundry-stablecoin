3a3b9d37d977085dee0aa98d365fa878a4bf9146
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2025-01-26 17:25:14 +0100

    feat: base functions for dscengine

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
@@ -28,15 +27,21 @@ pragma solidity ^0.8.19;
 /**
  * @title DSCEngine
  * @author Wozacosta
- * 
+ *
  * The system is designed to be as minimal as possible, and have the tokens
  * maintain a 1 token == $1 peg.
  * This stablecoin has those properties:
  * - Exogenous Collateral
  * - Dollar pegged
  * - Algorithmically stable
+ *
  * It is similar to DAI if DAI had no governance, no fees, and was only
  * backed by wETH and wBTC.
+ *
+ * Our DSC system should always be "overcollateralized". At no point, should the value of
+ * all collateral < the $ backed value of all the DSC.
+ *
+ *
  * @notice This contract is the core of the Decentralized Stablecoin system. It handles all the logic
  * for minting and redeeming DSC, as well as depositing and withdrawing collateral.
  * @notice This contract is based on the MakerDAO DSS system // https://github.com/makerdao/dss
@@ -46,11 +51,34 @@ contract DSCEngine {
         // deposit collateral
         // mint DSC
     }
-    
+
+    /**
+     * @param tokenCollateralAddress The address of the token to deposit as collateral
+     * @param amountCollateral The amount of collateral to deposit
+     */
+    function depositCollateral(
+        address tokenCollateralAddress,
+        uint256 amountCollateral
+    ) external {
+        // deposit collateral
+    }
+
     function redeemCollateralForDSC() external {
-        // burn DSC
+        // redeem DSC
         // withdraw collateral
     }
-    
-}
 
+    function redeemCollateral() external {
+        // withdraw collateral
+    }
+
+    function mintDSC() external {
+        // mint DSC
+    }
+
+    function burnDSC() external {}
+
+    function liquidate() external {}
+
+    function getHealthFactor() external view {}
+}
